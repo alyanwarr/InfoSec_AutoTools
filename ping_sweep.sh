d9214f7c4a9be654b72a07d25ee35d495d754580
Aly Anwar <alyanwarr@gmail.com>
2017-07-13 21:53:52 +0200

    Update ping_sweep.sh

--- ping_sweep.sh
+++ ping_sweep.sh
@@ -7,7 +7,7 @@ now=$(date +%Y_%m_%d_%H_%M);
 filename="ping_sweep_$now"
 nmap -sP $subnet -n --max-rtt-timeout 50ms -oG - | awk '/Up$/{print $2}' > $filename;
 echo "[-] Results saved to $(pwd)/$filename";
-read -p "[-] View Results? (y/n) " -n 1 -r
+read -p "[-] View Saved Results? (y/n) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     cat $filename
